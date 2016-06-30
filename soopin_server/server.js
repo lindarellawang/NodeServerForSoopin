c72e5ea9f7863514d62efbb01a0a275c11d9c56d
lindarellawang <lindawg0118@gmail.com>
2016-07-01 05:47:05 +0900

    Public IP address update

--- soopin_server/server.js
+++ soopin_server/server.js
@@ -1,8 +1,8 @@
 var http = require('http');
-var hostname = 'localhost'
+var hostname = '52.41.175.55'
 http.createServer(function(req,res) {
 
     res.writeHead(200, {'Content-Type':'text/plain'});
     res.end('Hello Linda, I am Node server placed on Amazon EC2 Ubuntu server');
 }).listen(9000, hostname);
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at http://52.41.175.55:9000/');
