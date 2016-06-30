a2927e69c4c3642ac1bd5e842fa37f64e3e74738
lindarellawang <lindawg0118@gmail.com>
2016-07-01 06:48:55 +0900

    Node server for soopin client
    Installation

--- soopin_server/server.js
+++ soopin_server/server.js
@@ -1,8 +1,8 @@
 var http = require('http');
-var hostname = '172.31.37.42'
+var hostname = '52.41.175.55'
 http.createServer(function(req,res) {
 
     res.writeHead(200, {'Content-Type':'text/plain'});
     res.end('Hello Linda, I am Node server placed on Amazon EC2 Ubuntu server');
-}).listen(9000, hostname);
-console.log('Server running at http://172.31.37.42:9000/');
+}).listen(80);
+console.log('Server running at http://52.41.175.55:80/');
