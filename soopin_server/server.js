398439db535a44a66b3f63bcd7e52a6310abb983
lindarellawang <lindawg0118@gmail.com>
2016-07-01 07:16:00 +0900

    update

--- soopin_server/server.js
+++ soopin_server/server.js
@@ -4,5 +4,5 @@ http.createServer(function(req,res) {
 
     res.writeHead(200, {'Content-Type':'text/plain'});
     res.end('Hello Linda, I am Node server placed on Amazon EC2 Ubuntu server');
-}).listen(3000);
+}).listen(3000, hostname);
 console.log('Server running at http://52.41.175.55:80/');
